968eb3138a13f218bb55354f016a7f4c71355ab3
nchernyavsky <mikita1234qwer@gmail.com>
2023-09-02 16:56:49 +0300

    added file controller

--- src/modules/pokemon/pokemon.controller.ts
+++ src/modules/pokemon/pokemon.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipe,
+  FileTypeValidator,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 import { PokemonService } from './pokemon.service';
 import { SinglePokemonParam } from 'interfaces/pokemon.types';
@@ -15,5 +26,18 @@ export class PokemonController {
   @Get('type/:typeId')
   async findAllPokemon(@Param('typeId', ParseIntPipe) typeId: number) {
     return await this.pokemonService.getAllPokemonByType(typeId);
+  }
+
+  @Post('file')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new FileTypeValidator({ fileType: 'text/csv' })],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return await this.pokemonService.fileWithPokemon(file);
   }
 }
