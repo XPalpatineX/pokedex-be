3613d45c66ebab27f58b4a508df7b02726d63e1e
nchernyavsky <mikita1234qwer@gmail.com>
2024-09-06 00:42:02 +0200

    fix mongoose factory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,36 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerModule } from '@nestjs/throttler';
 
+import { AuthModule } from 'modules/auth/auth.module';
+import { PokemonModule } from 'modules/pokemon/pokemon.module';
+import { SessionModule } from 'modules/session/session.module';
+import { UsersModule } from 'modules/users/users.module';
+import * as path from 'node:path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PokemonModule } from 'modules/pokemon/pokemon.module';
-import { AuthModule } from 'modules/auth/auth.module';
-import { UsersModule } from 'modules/users/users.module';
-import { SessionModule } from 'modules/session/session.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ThrottlerModule.forRoot({
       ttl: 60,
       limit: 100,
     }),
-    MongooseModule.forRoot('mongodb://mongodb:27017/pokedex'),
-    // MongooseModule.forRootAsync({
-    //   useFactory: () => ({
-    //     uri: process.env.DATABASE_URL,
-    //   }),
-    // }),
-    ConfigModule.forRoot(),
+    // MongooseModule.forRoot('mongodb://mongodb:27017/pokedex'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        console.log(path.join(__dirname, '../global-bundle.pem'));
+        return {
+          uri: configService.get<string>('MONGODB_URI'),
+          lazyConnection: true,
+          tlsCAFile: path.join(__dirname, '../global-bundle.pem'),
+        };
+      },
+      inject: [ConfigService],
+    }),
     PokemonModule,
     AuthModule,
     UsersModule,
